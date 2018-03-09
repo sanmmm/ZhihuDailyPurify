83fc512367e3a2b2967c4a21e27b95d0aecac142
Xiao Liang <izzyliang@gmail.com>
2018-03-08 20:25:17 -0800

    Update smoke test code

--- news_fetch/server/smoke.py
+++ news_fetch/server/smoke.py
@@ -3,19 +3,25 @@ from six.moves.urllib import request
 URL_BASE = 'https://zhihudailypurify.herokuapp.com'
 
 """
-Get news
+Smoke index page
+"""
+index_page_request = request.urlopen(URL_BASE)
+assert(index_page_request.getcode() == 200)
+
+"""
+Smoke news page
 """
 news_request = request.urlopen(URL_BASE + '/news/20171122')
 assert(news_request.getcode() == 200)
 
 """
-Get news, but not from cache
+Smoke news page, but force fetching from Zhihu Daily
 """
 no_cache_news_request = request.urlopen(URL_BASE + '/news/20171122?bypass_cache=true')
 assert(no_cache_news_request.getcode() == 200)
 
 """
-Search
+Smoke search page.
 """
 search_request = request.urlopen(URL_BASE + '/search/?q=2017')
 assert(search_request.getcode() == 200)
